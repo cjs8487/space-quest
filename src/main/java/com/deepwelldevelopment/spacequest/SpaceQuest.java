576b48636fdc9a2bf9308b348bbaf273189c2de1
cjs8487 <cjschaefer1999@gmail.com>
2026-02-22 22:43:05 -0600

    initial project setup

---
+++ src/main/java/com/deepwelldevelopment/spacequest/SpaceQuest.java
@@ -0,0 +1,24 @@
+package com.deepwelldevelopment.spacequest;
+
+import org.tinylog.Logger;
+
+public class SpaceQuest {
+    private long window;
+    private int width = 800;
+    private int height = 600;
+
+    private void init() {
+    }
+
+    private void input() {
+    }
+
+    private void update() {}
+
+    private void cleanup() {
+    }
+
+    public static void main(String[] args) {
+        Logger.info("Starting Space Quest...");
+    }
+}
